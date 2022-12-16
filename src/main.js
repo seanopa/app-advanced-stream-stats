53b054060b529cb153983783e879848483490165
Taurai Muzopa <seanopa@gmail.com>
2022-12-16 18:16:11 -0500

    add routing and axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import http from './lib/http'
 
 Vue.config.productionTip = false
+Vue.prototype.$http = http;
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
